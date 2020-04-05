a7aae6cd7b3828872c7a480260dbf4e221049b76
Narendra Singh Rathore <rathoreofficialemail@gmail.com>
2020-04-05 17:22:15 +0530

    Container component page design.

--- src/App.js
+++ src/App.js
@@ -8,28 +8,37 @@ import "./App.css";
  */
 import Dashboard from "./components/statefull/Dashboard/Dashboard";
 import About from "./components/statefull/About/About";
-import Navigation from './components/stateless/Navigation/Navigation';
+import Navigation from "./components/stateless/Navigation/Navigation";
 import ThemeManager from "./components/stateless/ThemeManager/ThemeManager";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <ThemeManager/>
-        <Navigation />
-
-        {/* A <Switch> looks through its children <Route>s and
+    <div id="app" class="flex-container">
+      <Router>
+        <header>
+          <ThemeManager />
+          <Navigation />
+          Header
+        </header>
+        <main>
+          <aside>
+            <Navigation />
+          </aside>
+          <section>
+            {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </section>
+        </main>
+      </Router>
+    </div>
   );
 }
 
